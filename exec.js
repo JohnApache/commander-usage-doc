7f2a6020a4fe6cca046b8ca14abe70dfec0cb64e
cjw <1468733552@qq.com>
2019-08-26 22:31:24 +0800

    更新 commander demo

--- exec.js
+++ exec.js
@@ -27,16 +27,16 @@ console.log(program.happy);
 console.log(program.I);
 
 
-// program
-//     .command('init <templateName> [envs...]') // 创建命令 <> 必填参数 [] 选填参数 ... 可接收多个
-//     .alias('i') // 命令取别名
-//     .description('this is a init command') // 命令的描述
-//     .option('-j, --jade', 'this is a jade option') // 该命令相关的选项配置 
-//     .action((templateName, envs, cmdObj) => {
-//         console.log(templateName);
-//         console.log(envs); // 如果有命令两个参数 第二个参数就是该变量。 如果没有第二个参数 且包含 option 选项配置的时候 第二个参数就是 command 本身
-//         console.log(cmdObj.opts()); // 如果有两个参数 且包含option  ，第三个参数就是command本身
-//     });
+program
+    .command('init <templateName> [envs...]') // 创建命令 <> 必填参数 [] 选填参数 ... 可接收多个
+    .alias('i') // 命令取别名
+    .description('this is a init command') // 命令的描述
+    .option('-j, --jade', 'this is a jade option') // 该命令相关的选项配置 
+    .action((templateName, envs, cmdObj) => {
+        console.log(templateName);
+        console.log(envs); // 如果有命令两个参数 第二个参数就是该变量。 如果没有第二个参数 且包含 option 选项配置的时候 第二个参数就是 command 本身
+        console.log(cmdObj.opts()); // 如果有两个参数 且包含option  ，第三个参数就是command本身
+    });
 
 program
     .arguments('<cmd> [envs...]') // 匹配任意 命令 且, 命令即参数被当作参数传入 action
@@ -47,15 +47,6 @@ program
    
 // git style command
 program
-    .command('init <templateName> [envs...]') // 创建命令 <> 必填参数 [] 选填参数 ... 可接收多个
-    .alias('i') // 命令取别名
-    .description('this is a init command') // 命令的描述
-    .option('-j, --jade', 'this is a jade option') // 该命令相关的选项配置 
-    .action((templateName, envs, cmdObj) => {
-        console.log(templateName);
-        console.log(envs); // 如果有命令两个参数 第二个参数就是该变量。 如果没有第二个参数 且包含 option 选项配置的时候 第二个参数就是 command 本身
-        console.log(cmdObj.opts()); // 如果有两个参数 且包含option  ，第三个参数就是command本身
-    })
     .command('install <templateName>', 'this is git style command') // 默认查找 当前目录的 相关文件 [当前文件名]-install exec-install.js 并执行
     .command('publish <name>', 'this is git style command', { excutableFile: 'execPublish' }) // 当提供配置选项的时候 会执行 excutableFile 指定的文件  
     .command('default <name>', 'this is git style command', { isDefault: true }) // 当不传入命令的时候 或没有匹配到任意，命令时 会搜索 exec-defualt.js 并执行,
